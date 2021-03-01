2b119c1227d16f09e5f3bd3a7760797b1438678a
Gorkhalianil775 <punkanil97@gmail.com>
2021-03-01 14:15:43 -0600

    Header complete

--- my-website/src/Components/Navbar.js
+++ my-website/src/Components/Navbar.js
@@ -2,22 +2,21 @@ import React from "react";
 import logo from "../logo.png";
 //REACT FONTAWSOME IMPORTS
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <nav className="navbar navbar-expand-lg bg-info" >
             <div className="container">
 
 
                 <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo.." /> </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" >
-                    <span className="navbar-toggler-icon">
-                    </span>
+                    <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }} />
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
+                    <ul className="navbar-nav ml-auto" >
                         <li className="nav-item active">
                             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
